50c01497dd6317eaa31fe485a281adad7a78fcfd
isomakoo <161478022+isomakoo@users.noreply.github.com>
2024-04-04 15:03:00 +0500

    ibdex.js va index.html jsga oid misollar

---
+++ index.js
@@ -0,0 +1,83 @@
+// misol-1
+// let son=Number(prompt("sonni kiriting"))
+// if(son%2==0){
+//     console.log(son**2,"sonning kavadrati")
+// }
+// else{
+//     console.log(Math.sqrt(son),"sonning ildizi")
+// }
+// misol-2
+// let a=String(prompt("matn kiriting"))
+// if(a.length>10){
+//     console.log(a.toLocaleUpperCase())
+// }
+// else{
+//     console.log(a.toLowerCase())
+// }
+// 3-misol
+// let a=String(prompt("matn kiriting"))
+// let b=String(prompt("matn kiriting"))
+// if(a.length>15){
+//     console.log(a.concat(" ", b))
+// }
+// else console.log("matn uzunligi 15 dan kichik")
+// 4-misol
+// let a=String(prompt("matn kiriting"))
+// if(a.length>5){
+//     console.log(a.repeat(3))
+// }
+// else console.log("kiritilgan matn 5 harfdan kichik")
+// 5-misol
+// let a=String(prompt("matn kiriting"))
+// if(a.includes("a")){
+//     console.log(a.replace("a", "&"))
+// }
+// else console.log("kiritilgan matn ichida a harfi yuq")
+// 6-misol
+// let a=String(prompt("matn kiriting"))
+// if(a.length>10){
+//     console.log(a.slice(5))
+// }
+// else console.log("kiritilgan matn uzunligi 10 dan kichik")
+// 7-misol
+// let a=Number(prompt("sonni kiriting"))
+// console.log(a.toString())
+// 8-misol
+// let a=String(prompt("matn kiriting"))
+// console.log(a.trim())
+// 9-misol
+// let a=Number(prompt("matn kiriting"))
+// if(a<0){
+//     console.log(Math.abs(a))
+// }
+// else console.log("kiritilgan son musbat")
+// 10-misol
+// let a=String(prompt("matn kiriting"))
+// if(a.length>20){
+//     console.log(a.slice(3))
+// }
+// else console.log("kiritilgan matn uzunligi 20 dan kickina")
+// 11-misol
+// let a=String(prompt("matn kiriting"))
+// if(a.includes("a"), a.includes("b")){
+//     console.log(a.replace("a", "A"))
+// }
+// else console.log("kiritilgan matnda a va b harf yuq")
+// 12-misol
+// let a=String(prompt("matn kiriting"))
+// if(a.includes("java")){
+//     console.log(a.replace("java", "JavaScrip"))
+// }
+// else console.log("matnda java suz yuq")
+// 13-misol
+// let a=String(prompt("matn kiriting"))
+// if(a.length>15){
+//     console.log(a.slice(10), 'hello TATU')
+// }
+// else console.log("matnda 15 ta harf qatnashmagan")
+// 14-misol
+// let a=Number(prompt("sonni kiriting"))
+// if(a<0){
+//     console.log(a," ",a, " ", a,)
+// }
+// else console.log(a+10)
